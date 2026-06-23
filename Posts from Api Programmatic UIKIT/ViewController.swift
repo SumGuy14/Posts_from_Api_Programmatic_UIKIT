3bc4ee7e5dddfa59b7bf3d899f41c7dc4e4ef93d
SumGuy14 <sumedhkulkarni14@gmail.com>
2026-06-23 14:44:05 -0400

    Initial Commit

---
+++ Posts from Api Programmatic UIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posts from Api Programmatic UIKIT
+//
+//  Created by Sumedh Kulkarni on 6/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Posts from Api Programmatic UIKITUITests/Posts_from_Api_Programmatic_UIKITUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  Posts_from_Api_Programmatic_UIKITUITestsLaunchTests.swift
+//  Posts from Api Programmatic UIKITUITests
+//
+//  Created by Sumedh Kulkarni on 6/23/26.
+//
+
+import XCTest
+
+final class Posts_from_Api_Programmatic_UIKITUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
